93c5b92521eeb626a6fabc334ce75a125ac814ad
pericarney00 <pericarney@gmail.com>
2025-08-14 11:17:59 -0500

    Created pet list and rendered to ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,41 @@
 import { useState, useEffect } from 'react'
 import * as petService from "./services/petService.js"
 import './App.css'
+import PetList from "./components/PetList/PetList.jsx"
+
 
 function App() {
   const [pets, setPets] = useState([])
 
-
+  useEffect( () => {
+    const fetchPets = async () => {
+      try {
+        const fetchedPets = await petService.index()
+        if (fetchedPets.error) {
+          throw new Error(fetchedPets.error)
+        }  
+        setPets(fetchedPets)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchPets()
+}, [])
 
 
 
   return (
 
-    < h1 > Hello world!</h1 >
-  )
+    <>
+      <div>
+      <PetList pets={pets} />
+      </div>
+    
+    </>
+  
+  );
     
   
-}
+};
 
 export default App
--- src/services/petService.js
+++ src/services/petService.js
@@ -4,7 +4,7 @@ const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/pets`
 const index = async () => {
   try { 
     const res = await fetch(BASE_URL); //response object
-  return res.json(); //resolves the response
+    return res.json(); //resolves the response
   } catch (error) {
     console.log(error)
   }
